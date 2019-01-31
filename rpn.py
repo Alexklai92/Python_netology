6a28007019c6e049a715407e459c6211365066ad
Alexklai92 <44439432+Alexklai92@users.noreply.github.com>
2019-01-31 09:19:07 +0300

    Add files via upload

--- rpn.py
+++ rpn.py
@@ -1,10 +1,16 @@
 lst = input().split()
 
+for i in lst:
+	if len(lst) < 2:
+		lst.pop(0)
+		for j in i:
+			lst.append(j)
+				
 if len(lst) > 3:
 	raise SystemError('Много аргументов')
 			
 
-assert lst[0] == '+' or lst[0] == '-' or lst[0] == '*' or lst[0] == '/', 'Что то пошло не так'
+assert lst[0] == '+' or lst[0] == '-' or lst[0] == '*' or lst[0] == '/', f' Ваш оператор {lst[0]}'
 
 
 try:
