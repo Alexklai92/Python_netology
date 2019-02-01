0d2ba9d8228d3c3703592d4c8e25a9fd1f956ed9
Alexklai92 <44439432+Alexklai92@users.noreply.github.com>
2019-02-01 11:22:22 +0300

    Add files via upload

--- write_file_v2.py
+++ write_file_v2.py
@@ -1,8 +1,4 @@
-
-
 with open('recept.txt') as f:
-	
-
 	def add_cook_book():
 		cook_book = dict()
 		for line in f:
@@ -14,23 +10,17 @@ with open('recept.txt') as f:
 				ing = {'ingridient_name': rec[0], 'quantity': rec[1], 'measure': rec[2]}
 				cook_book[recept].append(ing)
 			f.readline()
-		
-		def test():
-			print(cook_book)
 		return cook_book
+		
 	def get_shop_list_by_dishes(dishes, person_count):
 		rmk = list()
 		n_rec = dict()
 		rmk = dishes
 		cbk = add_cook_book()
-	
 		for i in cbk.keys():
-
 			for g in rmk:
-
 				if g == i:
 					for j in cbk[g]:
-
 						j_dict = {'quantity': int(j['quantity'])  * person_count, 'measure': j['measure']}
 						n_rec.setdefault(j['ingridient_name'], j_dict)
 		print(n_rec)	
